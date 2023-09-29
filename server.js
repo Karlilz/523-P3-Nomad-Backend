cc19cf5503935798485d4d887ed16071864aca80
mconil <mconil5988@gmail.com>
2023-09-28 22:54:08 -0400

    backend completed

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
+const expressLayouts = require('express-ejs-layouts')
 const morgan = require("morgan");
 const cors = require("cors");
 const travelBlogController = require ('./controller/travelBlogController')
 
-
+app.use(express.static("public"));
+app.set("view engine", "ejs")
+app.use(expressLayouts)
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended:true}));
 app.use('/travelblog', travelBlogController)
 
-
+app.get("/", (req,res) =>{
+    res.render("home.ejs")
+})
 
 app.listen(PORT,() => console.log("Testing Travel Blog", PORT))
