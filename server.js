ae5bf83bb2159f648c465b830bc0eef394c79f86
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-30 12:35:53 -0400

    name change to server.js app use app name

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const expressLayouts = require('express-ejs-layouts')
 const morgan = require("morgan");
 const cors = require("cors");
-const travelBlogController = require ('./controller/travelBlogController')
+const postController = require ('./controller/postController')
 
 app.use(express.static("public"));
 app.use(expressLayouts)
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended:true}));
-app.use('/travelblog', travelBlogController)
+app.use('/travelblog', postController)
 
 app.get("/", (req,res) =>{
     res.send("Testing Nom@d App")
