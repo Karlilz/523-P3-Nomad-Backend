bc7109d826e0b7aef025558ff2d233f15cf54b80
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-30 12:10:21 -0400

    added username to post schema

--- model/TravelUser.js
+++ model/TravelUser.js
@@ -1,7 +1,8 @@
 const mongoose = require('../database/connection');
 
 const travelUserSchema = new mongoose.Schema({
-    name: String,
+    title: String,
+    username: String,
     location: String,
     image: String,
     comment: String,
